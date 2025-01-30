555ba0d627058f5933b5cd548fa94e7275c4ebd8
Samuli Seppänen <samuli.seppanen@gmail.com>
2025-01-30 12:43:20 +0000

    Support for Podman Quadlets
    Signed-off-by: Samuli Seppänen <samuli.seppanen@gmail.com>

---
+++ nginx/certbot-renew.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+#
+set -e
+
+. ../.env.prod.nginx
+
+CMD=""
+COMPOSE_CMD=""
+
+# Use podman if available, docker otherwise
+if which -s podman; then
+    CMD="podman"
+    COMPOSE_CMD="podman-compose"
+    COMPOSE_ARG=""
+elif which -s docker; then
+    CMD="docker"
+    COMPOSE_CMD="$CMD"
+    COMPOSE_ARG="compose"
+else
+    echo "ERROR: neither podman nor docker found!"
+    exit 1
+fi
+
+if ! which -s $CMD; then echo "ERROR command $CMD not found!"; exit 1; fi
+if ! which -s $COMPOSE_CMD; then echo "ERROR compose command $COMPOSE_CMD not found!"; exit 1; fi
+
+CWD=$(pwd)
+
+cd $OTTERWIKI_CONTEXT
+
+# Commands below may return a non-zero exit code by design
+set +e
+
+# This requires that port redirection is done on the host from port 80 to port 8080
+$CMD run --rm --name certbot -p 8080:80 -v "${OTTERWIKI_VOLUME}:/etc/letsencrypt:rw,Z" certbot/certbot:latest certonly --standalone --non-interactive --agree-tos --email $OTTERWIKI_CERTBOT_EMAIL -d $OTTERWIKI_DOMAIN -v | grep "Successfully received certificate"
+
+if [ $? -eq 0 ]; then
+    $COMPOSE_CMD $COMPOSE_ARG -f docker-compose.prod.yml restart nginx 2>/dev/null || echo "nginx container not running, not restarting it"
+fi
+
+cd $CWD
